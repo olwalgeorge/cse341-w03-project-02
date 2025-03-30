11792f45b2877bd28476a481e6471705dcbedead
George Olwal <olwalgeorge@live.com>
2025-03-30 09:51:00 +0300

    errors to be merged

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,4 +1,5 @@
-const { check, param } = require("express-validator");
+const { check, param, body } = require("express-validator");
+const User = require("../models/user.model")
 
   
 const userValidateIdRules = () => {
@@ -23,15 +24,49 @@ const userValidateUserTypeRules = () => {
 };
 
 const userCreateValidationRules = () => {
-  return [    
-    
-    check("user_id", "User ID is required").notEmpty(), 
-    check("user_id", "User ID should start with 'usr_' then 4 digits").matches(/^usr_\d{4}$/),      
+  return [  
+    body('user_id')
+    .notEmpty()
+    .withMessage('User ID is required')
+    .trim()
+    .matches(/^usr_\d{4}$/)
+    .withMessage("User ID should start with 'usr_' then 4 digits")
+    .custom(async (value) => {
+      const userExists = await User.exists({ user_id: value });
+      if (userExists) {
+        throw new Error('User ID already exists');
+      }
+      return true;
+    }),    
+
+    body('username')
+    .notEmpty()
+    .withMessage('Username is required')
+    .trim()
+    .custom(async (value) => {
+      const usernameExists = await User.exists({ username: value });
+      if (usernameExists) {
+        throw new Error('Username already exists');
+      }
+      return true;
+    }),
+  body('email')
+    .notEmpty()
+    .withMessage('Email is required')
+    .isEmail()
+    .withMessage('Invalid email format')
+    .normalizeEmail()
+    .trim()
+    .custom(async (value) => {
+      const emailExists = await User.exists({ email: value });
+      if (emailExists) {
+        throw new Error('Email already exists');
+      }
+      return true;
+    }),    
     check("firstName", "First name is required").notEmpty(),
     check("lastName", "Last name is required").notEmpty(),
-    check("username", "Username is required").notEmpty(),    
-    check("email", "Email is required").notEmpty(),    
-    check("email", "Invalid email format").isEmail(),
+    check("username", "Username is required").notEmpty(),       
     check("password", "Password is required").notEmpty(),
     check("password", "Password must be at least 8 characters long").isLength({
       min: 8,

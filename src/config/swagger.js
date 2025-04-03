cba9ed3fe8c5b2c06189c3f626b4963bf4257f66
George Olwal <olwalgeorge@live.com>
2025-04-03 13:43:12 +0300

    swagger config

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,37 +1,31 @@
 // src/config/swagger.js
-const swaggerAutogen = require('swagger-autogen')();
-const config = require('./config');
-const logger = require('../utils/logger');
+const swaggerAutogen = require("swagger-autogen");
+const config = require("./config");
+const logger = require("../utils/logger");
+const { env,renderUrl } = config;
 
-const outputFile = '../../swagger_output.json'; 
-const endpointsFiles = ['./src/routes/index.js']; 
+const outputFile = "../../swagger_output.json";
+const endpointsFiles = ["../routes/index.js"];
+
+
+let host = env === "production" ? renderUrl : "localhost:3000";
+let schemes = env === "production" ? ["https"] : ["http"];
 
 const doc = {
   info: {
-    title: 'Smart Farm API',
-    description: 'API for a smart farm application',
+    title: "Smart Farm API",
+    description: "API documentation for the Smart Farm project",
   },
-  host: config.env === 'production' && config.renderUrl ? config.renderUrl : 'localhost:3000',
-  schemes: [config.env === 'production' ? 'https' : 'http'],
-  tags: [
-    {
-      name: 'Users',
-      description: 'Endpoints related to user management',
-    },
-    {
-      name: 'Sensors',
-      description: 'Endpoints related to sensor management',
-    },
-    {
-      name: 'Authentication',
-      description: 'Endpoints related to user authentication',
-    },
-  ],
+  host: host,
+  schemes: schemes,
 };
 
-swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-  require('../server'); 
-}).catch((err) => {
-  logger.error(err);
-});
-
+// Generate Swagger file on startup then run server
+swaggerAutogen(outputFile, endpointsFiles, doc)
+  .then(() => {
+    logger.info("Swagger documentation generated");
+    require("../server");
+  })
+  .catch((error) => {
+    logger.error("Error generating Swagger documentation:", error);
+  });
